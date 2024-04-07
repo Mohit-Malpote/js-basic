87f155f5de0008bfc44b212b28e8cfae879fe06c
Mohit-Malpote <165484421+Mohit-Malpote@users.noreply.github.com>
2024-04-07 14:59:14 +0000

    Opretors

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+let name = "Mohit"
+
+// console.log(name);
+// console.log(typeof name);
+
+let valueInNumber = Number(name);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); NaN
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Mohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn); true
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let onenumber = 41
+let stringnumber = String(onenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ******************Opretors**************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);add
+// console.log(2-2);sub
+// console.log(2*2);multiplay
+// console.log(2**3);2^3
+// console.log(2/3);div
+// console.log(2%3);remender
+
+let str1 = "hallo"
+let str2 = "Mohit"
+let str3 = str1 + str2
+// console.log(str3);
+
+/*console.log(1+2); //3
+console.log("1"+2); //12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32 */
+
+// console.log( (3 + 4) * 5 % 3);
+
+//   console.log(+true);
+//  console.log(+"");
+
+let num1 , num2 , num3
+num1=num2=num3= 2 + 2 
+// console.log(num3);
+
+let gameConter = 100
+++gameConter;
+console.log(gameConter);
+
+// Unary operator. Adds one to its operand. If used as a prefix operator (++x), returns the value of its operand after adding one; if used as a postfix operator (x++), returns the value of its operand before adding one
+ 
